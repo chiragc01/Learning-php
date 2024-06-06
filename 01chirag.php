d916a07ec2ea486dd70ccddd8935840b7924018f
Chirag <chiragchaudhary2607@gmail.com>
2024-06-06 21:45:14 +0530

    Todays progress(6/6/2024)

---
+++ 01chirag.php
@@ -0,0 +1,9 @@
+<?php
+echo "This is chirag.php";
+?>
+<?php
+echo "This is chirag.php";
+?>
+<?php
+echo "This is chirag.php";
+?>
---
+++ 04variables.php
@@ -0,0 +1,14 @@
+<?php
+
+// variables are container for storing information 
+// Starts with "$"
+// They ar case sensitive
+
+
+$name= "Chirag";
+$income= 2000;
+echo "my name is $name and my salary in $income";
+
+
+
+?>
